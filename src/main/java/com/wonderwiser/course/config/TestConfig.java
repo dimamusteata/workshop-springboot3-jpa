b88752e2fd754508ec6e44b3e393d969740a6fc3
dimamusteata <dmauu@iscte-iul.pt>
2025-08-15 12:17:22 +0100

    OrderStatus Enum

--- src/main/java/com/wonderwiser/course/config/TestConfig.java
+++ src/main/java/com/wonderwiser/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.wonderwiser.course.entities.Order;
 import com.wonderwiser.course.entities.User;
+import com.wonderwiser.course.entities.enums.OrderStatus;
 import com.wonderwiser.course.repositories.OrderRepository;
 import com.wonderwiser.course.repositories.UserRepository;
 
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com"	, "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com"	, "977777777", "123456");
 		
-		Order o1 = new Order(null, Instant.parse("2025-06-20T19:52:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2025-07-21T09:12:50Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2025-07-22T11:32:11Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2025-06-20T19:52:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2025-07-21T09:12:50Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2025-07-22T11:32:11Z"), OrderStatus.DELIVERED, u1);
 		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
